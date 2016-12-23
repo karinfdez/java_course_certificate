241933c1303e1a0f9363e21aa1e4a588b029e995
Karin Fernandez <karinfdez@yahoo.com>
2016-12-22 23:50:36 -0500

    first commit

---
+++ unitTesting/third_assignment/techdegree-javaweb-unit-testing-0.0.3/src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -0,0 +1,62 @@
+package com.teamtreehouse.techdegree.overboard.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by karinfernandez on 12/22/16.
+ */
+public class UserTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void askQuestion() throws Exception {
+
+    }
+
+    @Test
+    public void answerQuestion() throws Exception {
+
+    }
+
+    @Test
+    public void acceptAnswer() throws Exception {
+
+    }
+
+    @Test
+    public void upVote() throws Exception {
+
+    }
+
+    @Test
+    public void downVote() throws Exception {
+
+    }
+
+    @Test
+    public void getReputation() throws Exception {
+
+    }
+
+    @Test
+    public void getQuestions() throws Exception {
+
+    }
+
+    @Test
+    public void getAnswers() throws Exception {
+
+    }
+
+    @Test
+    public void getName() throws Exception {
+
+    }
+
+}
