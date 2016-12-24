373689c346c1d764b373c5e74fe1fb90588b8d9a
Karin Fernandez <karinfdez@yahoo.com>
2016-12-24 12:52:41 -0500

    passed first test

--- unitTesting/third_assignment/techdegree-javaweb-unit-testing-0.0.3/src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ unitTesting/third_assignment/techdegree-javaweb-unit-testing-0.0.3/src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -9,8 +9,18 @@ import static org.junit.Assert.*;
  * Created by karinfernandez on 12/22/16.
  */
 public class UserTest {
+    private User alice;
+    private User bob;
+    private User charles;
+
+
     @Before
     public void setUp() throws Exception {
+
+        Board board = new Board("Java");
+        alice = board.createUser("alice");
+        bob = board.createUser("bob");
+        charles = board.createUser("charles");
 
     }
 
@@ -30,8 +40,10 @@ public class UserTest {
     }
 
     @Test
-    public void upVote() throws Exception {
-
+    public void reputationGoesUpIfQuestionUpvoted() throws Exception {
+        Question question = alice.askQuestion("What is a String?");
+        bob.upVote(question);
+        assertEquals("Reputation doesn't goes up by 5",5,alice.getReputation());
     }
 
     @Test
