d9cae05494e1e354d06cc06d44d97b79cb92c4f9
Karin Fernandez <karinfdez@yahoo.com>
2016-12-24 14:13:52 -0500

    added extra credit test

--- unitTesting/third_assignment/techdegree-javaweb-unit-testing-0.0.3/src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ unitTesting/third_assignment/techdegree-javaweb-unit-testing-0.0.3/src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -13,6 +13,7 @@ import static org.junit.Assert.*;
 public class UserTest {
     private User alice;
     private User bob;
+    private User charlie;
     private Question question;
     private Answer bobAnswer;
     private Post bobPost;
@@ -23,17 +24,22 @@ public class UserTest {
         Board board = new Board("Java");
         alice = board.createUser("alice");
         bob = board.createUser("bob");
+        charlie = board.createUser("charlie");
         question = alice.askQuestion("What is a String?");
         bobAnswer=bob.answerQuestion(question,"A sequence of characters");
         bobPost=bob.answerQuestion(question,"A sequence of characters");
 
     }
 
+    //Write a test to ensure that the questioner’s reputation goes up by 5 points if their question is upvoted.
     @Test
+
     public void reputationGoesUpIfQuestionUpvoted() throws Exception {
         bob.upVote(question);
         assertEquals("Reputation doesn't goes up by 5",5,alice.getReputation());
     }
+
+    //Write a test to assert that the answerer’s reputation goes up by 10 points if their answer is upvoted.
 
     @Test
     public void reputationGoesUpIfAnswerUpvoted() throws Exception {
@@ -41,11 +47,16 @@ public class UserTest {
         assertEquals("Answer's reputation doesn't goes up by 10",10,bob.getReputation());
     }
 
+    //Write a test that proves that having an answer accepted gives the answerer a 15 point reputation boost
+
     @Test
     public void acceptedAnswerGivesAnswererReputationPoints() throws Exception {
         alice.acceptAnswer(bobAnswer);
         assertEquals("Answer's reputation doesn't goes up by 15",15,bob.getReputation());
     }
+
+    //Using a test, ensure that voting either up or down is not allowed on questions or answers by the original author,
+    // you know to avoid gaming the system. Ensure the proper exceptions are being thrown.
 
     @Test(expected= VotingException.class)
     public void VotingMadeByAuthorThrowsException() throws Exception {
@@ -55,8 +66,21 @@ public class UserTest {
         alice.downVote(question);
     }
 
+    //Write a test to make sure that only the original questioner can accept an answer.
+    // Ensure the intended messaging is being sent to back to the caller.
+
     @Test(expected= AnswerAcceptanceException.class)
     public void originalQuestionerCanAcceptAnswer() throws Exception {
         bob.acceptAnswer(bobAnswer);
+    }
+
+    //Reviewing the User.getReputation method may expose some code that is not requested to
+    // be tested in the Meets Project instructions. Write the missing test(EXTRA CREDIT).
+
+    @Test
+    public void downVotingQuestionsGetNoPoints() throws Exception {
+        bob.upVote(question);
+        charlie.downVote(question);
+        assertEquals(5,alice.getReputation());
     }
 }
